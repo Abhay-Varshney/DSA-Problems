0d05a1599db14de04ae8165e5c2b8f6aaa7ae708
Abhay-Varshney <abhayvarshney24@gmail.com>
2026-04-07 19:34:28 +0530

    ARRAYS

---
+++ ARRAYS/Merge_Overlapping_subIntervals.py
@@ -0,0 +1,29 @@
+class Solution:
+    def Merge_overlapping_subIntervals(self, interval):
+        n=len(interval)
+        visited=[False]*n
+        result=[]
+        
+        for i in range(n):
+            if visited[i]:
+                continue
+            s,e=interval[i]
+            
+            for j in range(i+1,n):
+                if visited[j]:
+                    continue
+                s2,e2=interval[j]
+                
+                if not(e<=s2 or e2<=s):
+                    s=min(s,s2)
+                    e=max(e,e2)
+                    visited[j]=True
+            result.append([s,e])
+            result.sort()
+        return result
+interval = [[1,5],[3,6],[8,10],[15,18]]
+obj = Solution()
+print(obj.Merge_overlapping_subIntervals(interval))
+
+
+'''[[1, 6], [8, 10], [15, 18]]'''
