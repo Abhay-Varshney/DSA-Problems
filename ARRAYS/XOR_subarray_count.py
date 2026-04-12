dd13a803b7437cc4307ebf099639125932a674a2
Abhay-Varshney <abhayvarshney24@gmail.com>
2026-04-12 21:45:58 +0530

    ARRAYS

---
+++ ARRAYS/XOR_subarray_count.py
@@ -0,0 +1,16 @@
+class Solution:
+    def XOR_subarray_count(self, arr, k):
+        count=0
+        for i in range(len(arr)):
+            xor=0
+            for j in range(i,len(arr)):
+                xor^=arr[j]
+                if xor==k:
+                    count+=1
+        return count
+arr = [4, 2, 2, 6, 4]
+obj = Solution()
+print(obj.XOR_subarray_count(arr,6))
+
+
+'''4'''
