8758a5aaa5e59bd3aa76948ac2d8a1585da099b4
Abhay-Varshney <abhayvarshney24@gmail.com>
2026-04-18 22:55:37 +0530

    ARRAYS

---
+++ ARRAYS/Nth_root.py
@@ -0,0 +1,23 @@
+class Solution:
+    def Nth_root(self,n,m):
+        if n==0 or n==1:
+            return n
+        low,high=1,n
+        result=1
+        while low<=high:
+            mid=(low+high)//2
+            if mid**m==n:
+                return mid
+            elif mid**m<n:
+                result=mid
+                low=mid+1
+            else:
+                high=mid-1
+        return result
+n=216
+obj = Solution()
+print(obj.Nth_root(n,3))
+
+
+
+'''6'''
